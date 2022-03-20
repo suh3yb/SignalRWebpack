d959114f385818a41b2bcecd9d2e2e760948859e
Suheyb Coskun <suheyb.c@gmail.com>
2022-03-20 14:22:37 +0100

    introduce nr of users and other functionalities

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -4,6 +4,47 @@ namespace SignalRWebpack.Hubs;
 
 public class ChatHub : Hub
 {
-    public async Task NewMessage(long username, string message) =>
-        await Clients.All.SendAsync("messageReceived", username, message);
+  public static class UserHandler
+  {
+    public static HashSet<string> ConnectedIds = new HashSet<string>();
+  }
+
+  public async Task NewMessage(long username, string message, string? connectionId = null, bool isPrivate = false)
+  {
+    if (connectionId != null && isPrivate)
+    {
+      var privateMessage = "Your username: " + username + "\nYour connectionId: " + connectionId + "\nYour message: " + message;
+      await Clients.Client(connectionId).SendAsync("chatMessageReceived", "SERVER (Private to you)", privateMessage);
+    }
+    else
+    {
+      await Clients.All.SendAsync("chatMessageReceived", username, message);
+    }
+  }
+
+  public async Task ServerMessage(long username)
+  {
+    var message = "Server says hi to " + username;
+    await Clients.All.SendAsync("serverMessageReceived", message);
+  }
+
+  public async Task Test(long username)
+  {
+    var message = username + " triggered a test.";
+    await Clients.All.SendAsync("serverMessageReceived", message);
+  }
+
+  public override Task OnConnectedAsync()
+  {
+    UserHandler.ConnectedIds.Add(Context.ConnectionId);
+    Clients.All.SendAsync("nrOfUsersReceived", UserHandler.ConnectedIds.Count);
+    return base.OnConnectedAsync();
+  }
+
+  public override Task OnDisconnectedAsync(Exception? exception)
+  {
+    UserHandler.ConnectedIds.Remove(Context.ConnectionId);
+    Clients.All.SendAsync("nrOfUsersReceived", UserHandler.ConnectedIds.Count);
+    return base.OnDisconnectedAsync(exception);
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,46 @@
-import * as signalR from '@microsoft/signalr';
+import { HubConnectionBuilder } from '@microsoft/signalr';
 import './css/main.css';
 
+const nrOfUsersNode: HTMLDivElement = document.querySelector('#nrOfUsers');
 const divMessages: HTMLDivElement = document.querySelector('#divMessages');
 const tbMessage: HTMLInputElement = document.querySelector('#tbMessage');
 const btnSend: HTMLButtonElement = document.querySelector('#btnSend');
+const btnServerTest: HTMLButtonElement =
+  document.querySelector('#btnServerTest');
+const btnServer: HTMLButtonElement = document.querySelector('#btnServer');
+const btnSendPrivateSelf: HTMLButtonElement = document.querySelector(
+  '#btnSendPrivateSelf'
+);
 const username = new Date().getTime();
 
-const connection = new signalR.HubConnectionBuilder().withUrl('/hub').build();
+const connection = new HubConnectionBuilder()
+  .withUrl('/hub')
+  .withAutomaticReconnect()
+  .build();
 
-connection.on('messageReceived', (username: string, message: string) => {
+connection.on('chatMessageReceived', (username: string, message: string) => {
   const m = document.createElement('div');
 
-  m.innerHTML = `<div class="message-author">${username}</div><div>${message}</div>`;
+  m.innerHTML = `
+    <div class="message-author">${username}</div>
+    <div class="message">${message}</div>
+    `;
 
-  divMessages.appendChild(m);
-  divMessages.scrollTop = divMessages.scrollHeight;
+  appendChild(m);
+});
+
+connection.on('serverMessageReceived', (message: string) => {
+  const m = document.createElement('div');
+
+  m.innerHTML = `
+    <div class="server-message">${message}</div>
+    `;
+
+  appendChild(m);
+});
+
+connection.on('nrOfUsersReceived', (nrOfUsers: number) => {
+  nrOfUsersNode.innerText = nrOfUsers.toString();
 });
 
 connection.start().catch(err => document.write(err));
@@ -26,9 +52,37 @@ tbMessage.addEventListener('keyup', (e: KeyboardEvent) => {
 });
 
 btnSend.addEventListener('click', send);
+btnServerTest.addEventListener('click', test);
+btnServer.addEventListener('click', server);
+btnSendPrivateSelf.addEventListener('click', sendPrivateSelfMessage);
+
+function appendChild(child: HTMLDivElement) {
+  divMessages.appendChild(child);
+  divMessages.scrollTop = divMessages.scrollHeight;
+}
 
 function send() {
   connection
-    .send('newMessage', username, tbMessage.value)
+    .send('newMessage', username, tbMessage.value, null, false)
+    .then(() => (tbMessage.value = ''));
+}
+
+function test() {
+  connection.send('test', username);
+}
+
+function server() {
+  connection.send('serverMessage', username);
+}
+
+function sendPrivateSelfMessage() {
+  connection
+    .send(
+      'newMessage',
+      username,
+      tbMessage.value,
+      connection.connectionId,
+      true
+    )
     .then(() => (tbMessage.value = ''));
 }
